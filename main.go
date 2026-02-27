aa1793c330e505d63a2d956d71a55f658c2346a0
Brian Ly <brianly1003@gmail.com>
2026-02-28 02:42:47 +0700

    chore: remove frontend directory

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"embed"
-
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 	"github.com/wailsapp/wails/v2/pkg/options/mac"
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
-
-//go:embed all:frontend/dist
-var assets embed.FS
 
 func main() {
 	// Create the desktop app which wraps Portal Core
@@ -33,12 +27,9 @@ func main() {
 		StartHidden:       false,
 		HideWindowOnClose: false,
 		BackgroundColour:  bgColor,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
-		},
-		OnStartup:  app.startup,
-		OnShutdown: app.shutdown,
-		OnDomReady: app.domReady,
+		OnStartup:         app.startup,
+		OnShutdown:        app.shutdown,
+		OnDomReady:        app.domReady,
 		Bind: []interface{}{
 			app,
 		},
